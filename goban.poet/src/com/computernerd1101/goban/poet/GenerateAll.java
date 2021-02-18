45ad7db4f0098aac4d1afdc3d9bf00b2fbb52f91
computernerd1101 <computernerd1101@gmail.com>
2021-02-18 11:14:43 -0700

    minor changes

--- goban.poet/src/com/computernerd1101/goban/poet/GenerateAll.java
+++ goban.poet/src/com/computernerd1101/goban/poet/GenerateAll.java
@@ -1,13 +1,18 @@
 package com.computernerd1101.goban.poet;
 
+import kotlin.collections.ArraysKt;
+
 public class GenerateAll {
 
     public static void main(String[] args) {
+        String[] reversePreview;
+        if (ArraysKt.contains(args, "--preview"))
+            reversePreview = new String[0];
+        else reversePreview = new String[] { "--no-preview" };
         GenerateGobanRowsKt.main();
         GenerateDateTablesKt.main();
-        String[] noPreview = { "--no-preview" };
-        GenerateTreeViewKt.main(noPreview);
-        GenerateToolbarKt.main(noPreview);
+        GenerateTreeViewKt.main(reversePreview);
+        GenerateToolbarKt.main(reversePreview);
     }
 
 }
