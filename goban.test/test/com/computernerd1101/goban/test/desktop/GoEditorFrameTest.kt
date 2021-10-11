ca1c16c85d2c20f840f00761b0f7243f9765c434
computernerd1101 <computernerd1101@gmail.com>
2021-10-11 15:12:03 -0600

    Added test.sgf

--- goban.test/test/com/computernerd1101/goban/test/desktop/GoEditorFrameTest.kt
+++ goban.test/test/com/computernerd1101/goban/test/desktop/GoEditorFrameTest.kt
@@ -3,20 +3,20 @@ package com.computernerd1101.goban.test.desktop
 import com.computernerd1101.goban.desktop.GoEditorFrame
 import com.computernerd1101.goban.sgf.GoSGF
 import com.computernerd1101.sgf.SGFException
-import java.io.FileInputStream
-import java.io.IOException
+import java.io.*
 import javax.swing.SwingUtilities
+import kotlin.reflect.jvm.javaMethod
 
-fun main(args: Array<String>) {
-    val sgf = (if (args.isNotEmpty()) readSGF(args[0]) else null) ?: GoSGF()
+fun main() {
+    val sgf = InputStream::readSGF.javaMethod!!.declaringClass.getResourceAsStream("test.sgf")!!.readSGF()
     SwingUtilities.invokeLater {
         GoEditorFrame(sgf).isVisible = true
     }
 }
 
-fun readSGF(file: String): GoSGF? {
+fun InputStream.readSGF(): GoSGF {
     return try {
-        FileInputStream(file).use { input ->
+        use { input ->
             GoSGF(input)
         }
     } catch(e: IOException) {
@@ -25,5 +25,5 @@ fun readSGF(file: String): GoSGF? {
     } catch(e: SGFException) {
         e.printStackTrace()
         null
-    }
+    } ?: GoSGF()
 }
